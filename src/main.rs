53842e1f71e89f26e9c1b721be16aee4c926f2b0
Amanse <amansetiarjp@gmail.com>
2022-12-27 18:37:16 +0530

    working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use] extern crate rocket;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize };
+use serde_json::Value;
 use rocket::http::Header;
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
@@ -23,14 +24,18 @@ impl Fairing for CORS {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct Res {
     results: Vec<Song>
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct Song {
+    id: String,
+    primary_artists: String,
+    image: String,
     media_preview_url: String,
+    song: String,
 }
 
 #[options("/one/<title>")]
@@ -47,7 +52,30 @@ fn hello(title: &str) -> String {
 
 }
 
+#[get("/search/<title>")]
+fn all(title: &str) -> String {
+    let res: Res = ureq::get(&format!("https://www.jiosaavn.com/api.php?_format=json&n=5&p=1&_marker=0&ctx=android&__call=search.getResults&q={}", title))
+        .call().unwrap()
+        .into_json().unwrap();
+     serde_json::to_string(&res).unwrap()
+}
+
+#[get("/id/<id>")]
+fn get_by_id(id: &str) -> String {
+    let res = ureq::get(&format!("https://www.jiosaavn.com/api.php?cc=in&_marker=0%3F_marker%3D0&_format=json&model=Redmi_5A&__call=song.getDetails&pids={}", id))
+            .call().unwrap()
+            .into_string().unwrap();
+    let res:Value = serde_json::from_str(&res).unwrap();
+    let res: Value = res.get(id).unwrap().clone();
+    let song: Song = serde_json::from_value(res).unwrap();
+    println!("{}", &song.media_preview_url);
+
+    format!("{}",&song.media_preview_url.replace("preview", "aac").replace("_96_p.mp4", "_320.mp4"))
+
+
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, coryy]).attach(CORS)
+    rocket::build().mount("/", routes![hello, coryy, all, get_by_id]).attach(CORS)
 }
